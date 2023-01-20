8cb0e049f0627aeffd194c27318e48a226e2212a
test <basile.pinsard@gmail.com>
2023-01-20 14:36:34 -0500

    add s8

---
+++ s8/cut_friends_s08e21.sh
@@ -0,0 +1,5 @@
+
+apptainer run     -B $PWD:/input     -B $PWD:/output     /data/neuromod/containers/melt.simg     -silent colour:black out=59     /input/s8/friends_s08e21.vob in=0 out=20068 -mix 60 -mixer luma     colour:black out=240 -mix 59 -mixer luma     -attach-track ladspa.1403 0=-25 1=0.25 2=0.4 3=0.6     -attach-track ladspa.1913 0=17 1=-3 2=0.5     -attach-track volume:-70db end=0db in=0 out=60     -attach-track volume:0db end=-70db in=20008 out=20068     -consumer avformat:/output/s8/friends_s08e21a.mkv f=matroska acodec=libmp3lame ab=256k vcodec=libx264 b=1500k
+        
+apptainer run     -B $PWD:/input     -B $PWD:/output     /data/neuromod/containers/melt.simg     -silent colour:black out=59     /input/s8/friends_s08e21.vob in=19888 out=40137 -mix 60 -mixer luma     colour:black out=240 -mix 59 -mixer luma     -attach-track ladspa.1403 0=-25 1=0.25 2=0.4 3=0.6     -attach-track ladspa.1913 0=17 1=-3 2=0.5     -attach-track volume:-70db end=0db in=0 out=60     -attach-track volume:0db end=-70db in=20188 out=20248     -consumer avformat:/output/s8/friends_s08e21b.mkv f=matroska acodec=libmp3lame ab=256k vcodec=libx264 b=1500k
+        
