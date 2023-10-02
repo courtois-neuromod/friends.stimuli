402dc17106fe0b7c7ace9a745c8ae47dc05b0a32
Isil Bilgin <bilginlab@gmail.com>
2023-10-02 16:53:09 -0400

    Update the notebook and data

--- language_annotations/friends_util.py
+++ language_annotations/friends_util.py
@@ -22,32 +22,20 @@ from collections import Counter
 from nltk.stem import SnowballStemmer, WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
 
-# def convert_to_data_frame(item_range, value, key_1, key_2):
-#     dictionary = {}
-#     for item in range(1, item_range):
-#         dictionary[item] = len(value[item])
-
-#     dataframe = pd.DataFrame({key_1: value.keys(), key_2: value.values()})
-#     return dataframe
-
 
 def load_csv_file(datapath: str):
     data = pd.read_csv(datapath, encoding="ISO-8859-1")
     seasons = data["season"]
     episodes = data["episode"]
-    corpus = data["line"]
+    corpus = data["dialogue"]
+    if "narrator" in data:
+        narrator = data["narrator"].str.replace(":", "")
+        narrator = narrator.str.capitalize()
+        return seasons, corpus, episodes, narrator, data
     return seasons, corpus, episodes, data
-
-    # if "narrator" in data:
-    #     narrator = data["narrator"]
-    #     return seasons, corpus, episodes, narrator
-    # else:
-    #     return seasons, corpus, episodes
 
 
 # found outlawed words
-
-
 def find_outlaw(word):
     """Find words that contain a same character 3+ times in a row."""
     is_outlaw = False
@@ -61,7 +49,7 @@ def find_outlaw(word):
 
 def word_characteristics(data):
     # concatanate all strings from all seasons
-    narrative = "".join(data["line"])
+    narrative = "".join(data["dialogue"])
     words = re.findall("\w+", narrative)
     unique_words = set(re.findall(r"\w+", narrative, re.UNICODE))
     return words, unique_words
@@ -71,14 +59,13 @@ def word_statistics(data):
     season_word = {}
     season_unique_word = {}
     freq_splits = {}
-    len_unique_words = []
-    len_word_per_season = []
+    unique_res = {}
+    outlaws = {}
     complete_narrative = []
-    season_range = list(range(1, 11))
 
     for season in range(1, 11):
         rslt_df = data[data["season"] == season]
-        narrative_per_season = "".join(rslt_df["line"])
+        narrative_per_season = "".join(rslt_df["dialogue"])
         docwords = re.findall("\w+", narrative_per_season)
         season_word[season] = docwords
         season_unique_word[season] = set(
@@ -86,12 +73,14 @@ def word_statistics(data):
         )
         freq_splits[season] = FreqDist(docwords)
         complete_narrative += docwords
+        outlaws[season] = [s for s in docwords if find_outlaw(s)]
+        res = np.array(outlaws[season])
+        unique_res[season] = np.unique(res)
+        random.shuffle(unique_res[season])
+    print(complete_narrative)
 
-    outlaws = [s for s in complete_narrative if find_outlaw(s)]
-    res = np.array(outlaws)
-    unique_res = np.unique(res)
-    random.shuffle(unique_res)
     freq_splits_all_seasons = FreqDist(complete_narrative)
+    print(freq_splits_all_seasons)
     frequent_word_list = []
     frequency_word_list = []
     for i in range(0, 20):
@@ -118,8 +107,9 @@ def stopword_statistics(tokens_norm):
 
 
 def token_statistics(data):
-    number_of_tokens = {}
     tokens_norm = {}
+    unique_tokens = {}
+    unique_tokens_list = {}
     tokens_all = []
     total_number_of_tokens = 0
     tokens_season = {}
@@ -128,43 +118,26 @@ def token_statistics(data):
     lemmatiser = WordNetLemmatizer()
     for season in range(1, 11):
         rslt_df = data[data["season"] == season]
-        narrative = "".join(rslt_df["line"])
+        narrative = "".join(rslt_df["dialogue"])
         tokens_season[season] = bpemb_en.encode(narrative)
         tokens_norm[season] = [
             lemmatiser.lemmatize(t.lower(), "v") for t in bpemb_en.encode(narrative)
         ]
+
+        # Use set to find unique tokens
+        unique_tokens[season] = set(tokens_season[season])
+
+        # Convert the set back to a list if needed
+        unique_tokens_list[season] = list(unique_tokens[season])
         total_number_of_tokens += len(tokens_season[season])
         tokens_all.append(tokens_season[season])
-    return tokens_season, tokens_norm, tokens_all, total_number_of_tokens
-
-
-def preprocess_text(rslt_df, stop_words):
-    """Preprocess the trslt_dfext to its purest form without any punctuation and stop words."""
-
-    narrative = "".join(rslt_df["line"])
-    tokeniser = RegexpTokenizer(r"[A-Za-z]{2,}")
-    tokens = tokeniser.tokenize(narrative)
-    # Lowercase and lemmatise
-    lemmatiser = WordNetLemmatizer()
-    lemmas = [lemmatiser.lemmatize(token.lower(), pos="v") for token in tokens]
-    # Remove stopwords
-    keywords = [lemma for lemma in lemmas if lemma not in stop_words]
-    return keywords
-
-
-def get_frequent_ngram(keywords, ngram, n=20):
-    """Find most common n n-grams tokens."""
-
-    # # Find ngrams per document
-    n_grams = list(ngrams(keywords, ngram))
-
-    # # Find frequency of ngrams
-    n_grams_flattened = [item for sublist in n_grams for item in sublist]
-    # freq_dist = FreqDist(n_grams)
-    # top_freq = freq_dist.most_common(n)
-    freq_dist = FreqDist(n_grams_flattened)
-    top_freq = freq_dist.most_common(n)
-    return pd.DataFrame(top_freq, columns=["ngram", "count"])
+    return (
+        tokens_season,
+        unique_tokens_list,
+        tokens_norm,
+        tokens_all,
+        total_number_of_tokens,
+    )
 
 
 def ngram_extractor(text, n_gram, stop_words):
@@ -191,6 +164,29 @@ def generate_ngrams(df, n_gram, max_row, stop_words):
     return temp_df
 
 
+def character_dialog_counts(narrator):
+    # remove characters who talks less than 1000 dialogues
+    count = Counter(narrator)
+    narrator_count = {}
+    i = 0
+    for j in count.keys():
+        if list(count.values())[i] > 200:
+            name = list(count.keys())[i]
+            name = re.sub(":", "", name)
+            name = name.capitalize()
+            narrator_count[name] = list(count.values())[i]
+        i += 1
+
+    sorted_narrator_count = sorted(narrator_count.items(), key=lambda x: x[1])
+
+    character_dialogues = {}
+
+    for i in sorted_narrator_count:
+        character_dialogues[i[0]] = i[1]
+
+    return narrator_count, sorted_narrator_count, character_dialogues
+
+
 def plot_corpus_characteristics(
     value, title: str, xlabel: str, ylabel: str, tilt: False, type: str
 ):
@@ -203,9 +199,9 @@ def plot_corpus_characteristics(
         sns.countplot(y=value)
     elif type == "ngram":
         fig, ax = plt.subplots()
-        y_pos = np.arange(len(value.keys())[0])
-        ax.barh(y_pos, list(value.keys())[1])
-        ax.set_yticks(y_pos, labels=list(value.keys())[1])
+        y_pos = np.arange(len(value))
+        ax.barh(y_pos, value[list(value.keys())[1]])
+        ax.set_yticks(y_pos, labels=value[list(value.keys())[0]])
         ax.invert_yaxis()
 
     plt.title(title, fontsize=12)
@@ -215,6 +211,3 @@ def plot_corpus_characteristics(
         plt.xticks(rotation=45)
 
     plt.show()
-
-
-# ['/home/isil/ComplexBrains/friends.stimuli/language_annotations', '/usr/lib/python310.zip', '/usr/lib/python3.10', '/usr/lib/python3.10/lib-dynload', '', '/home/isil/.local/lib/python3.10/site-packages', '/home/isil/ComplexBrains/friends_LLM_benchmark', '/usr/local/lib/python3.10/dist-packages', '/usr/lib/python3/dist-packages', '/usr/lib/python3/dist-packages/IPython/extensions', '/home/isil/.ipython']
